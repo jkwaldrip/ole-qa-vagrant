f1be1a72183113c38059aa27c529739f4b928d25
Jain Waldrip <jkwaldri@iu.edu>
2014-08-29 14:13:40 -0400

    Remove notify

--- manifests/install.pp
+++ manifests/install.pp
@@ -6,14 +6,14 @@ include rvm
 user { 'vagrant':
        ensure => present
 }
+
 rvm::system_user { vagrant: }
-notify {"Installing Ruby 2.1.2": }
-notify {"This may take some time": }
+
 rvm_system_ruby { 'ruby-2.1.2':
                     ensure => present,
                     default_use => true
 }
-notify {"Installing bundler for Ruby 2.1.2":}
+
 rvm_gem { 'bundler':
           name => 'bundler',
           ruby_version => 'ruby-2.1.2',
